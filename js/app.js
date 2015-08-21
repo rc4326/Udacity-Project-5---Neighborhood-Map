d8c132d450e3e75c5307557efcb5a8773b2e8262
rc4326 <bob75@comcast.net>
2015-08-20 22:20:50 -0500

    added comments
    added comments, cleaned up code

--- js/app.js
+++ js/app.js
@@ -6,98 +6,87 @@ var runLocations = [
         surface: "asphalt",
         trail: "bike path",
         popDense: "moderate",
-				icon: "img/marker_blue.png",
+        icon: "img/marker_blue.png",
         lat: 41.543218,
         lng: -88.161496
 
-    },
-		{
-				name: "I&M Canal",
-				surface: "crushed limestone",
-				trail: "towpath",
-				popDense: "light",
-				icon: "img/marker_blue.png",
-				lat: 41.485553,
-				lng: -88.163642
-		},
-		{
-			name: "Dellwood Park",
-			surface: "mixed",
-			trail: "nature",
-			popDense: "light",
-			icon: "img/marker_blue.png",
-			lat: 41.573131,
-			lng: -88.060688
-		}
+    }, {
+        name: "I&M Canal",
+        surface: "crushed limestone",
+        trail: "towpath",
+        popDense: "light",
+        icon: "img/marker_blue.png",
+        lat: 41.485553,
+        lng: -88.163642
+    }, {
+        name: "Dellwood Park",
+        surface: "mixed",
+        trail: "nature",
+        popDense: "light",
+        icon: "img/marker_blue.png",
+        lat: 41.573131,
+        lng: -88.060688
+    }
 ];
 
-var eatLocations = [
-{
+var eatLocations = [{
     name: "Chipotle",
     menu: "lunch dinner",
     type: "fast casual",
     cost: "$$$",
-		icon: "img/marker_green.png",
+    icon: "img/marker_green.png",
     lat: 41.522177,
     lng: -88.145398
 
-},
-{
-	name: "Portillos",
-	menu: "lunch, dinner",
-	type: "fast casual",
-	cost: "$$$",
-	icon: "img/marker_green.png",
-	lat: 41.543106,
-	lng: -88.199733
-},
-{
-	name: "Shooters Roadhouse",
-	menu: "dinner",
-	type: "casual dining",
-	cost: "$$$$",
-	icon: "img/marker_green.png",
-	lat: 41.521379,
-	lng: -88.203714
-},
-{
-	name: "Chicken n Spice",
-	menu: "lunch, dinner",
-	type: "fast food",
-	cost: "$$",
-	icon: "img/marker_green.png",
-	lat: 41.528891,
-	lng: -88.081630
-}
-];
+}, {
+    name: "Portillos",
+    menu: "lunch, dinner",
+    type: "fast casual",
+    cost: "$$$",
+    icon: "img/marker_green.png",
+    lat: 41.543106,
+    lng: -88.199733
+}, {
+    name: "Shooters Roadhouse",
+    menu: "dinner",
+    type: "casual dining",
+    cost: "$$$$",
+    icon: "img/marker_green.png",
+    lat: 41.521379,
+    lng: -88.203714
+}, {
+    name: "Chicken n Spice",
+    menu: "lunch, dinner",
+    type: "fast food",
+    cost: "$$",
+    icon: "img/marker_green.png",
+    lat: 41.528891,
+    lng: -88.081630
+}];
 
-var shopLocations = [
-{
+var shopLocations = [{
     name: "Louis Joliet Mall",
     type: "Retail Mall",
     price: "$$$$",
-		icon: "img/marker_purple.png",
+    icon: "img/marker_purple.png",
     lat: 41.576290,
     lng: -88.165566
 
-},
-{
-		name: "Hill Crest Shopping Center",
-		type: "Outdoor Mall",
-		price: "$$",
-		icon: "img/marker_purple.png",
-		lat: 41.552988,
-		lng: -88.127024
-},
-{
-	name: "Downtown Lockport",
-	type: "Boutique",
-	price: "$$$",
-	icon: "img/marker_purple.png",
-	lat: 41.589044,
-	lng: -88.057673
-}
-];
+}, {
+    name: "Hill Crest Shopping Center",
+    type: "Outdoor Mall",
+    price: "$$",
+    icon: "img/marker_purple.png",
+    lat: 41.552988,
+    lng: -88.127024
+}, {
+    name: "Downtown Lockport",
+    type: "Boutique",
+    price: "$$$",
+    icon: "img/marker_purple.png",
+    lat: 41.589044,
+    lng: -88.057673
+}];
 
 var ModelView = function() {
 
@@ -105,36 +94,36 @@ var ModelView = function() {
 
 
 
-		
+
 };
 
 var viewModel = function(map) {
-	var self = this;
-		
-		var markers = ko.observableArray([]);
-		
-		this.runClick = function() {
-			
-					runLocations.forEach(function(item) {
-				markers.push(new display_loc(item));
-			});
-		}
-		this.eatClick = function() {
-					 	eatLocations.forEach(function(item) {
-				markers.push(new display_loc(item));
-				});
-			};
-		
-		
-		this.shopClick = function() {
-					 	shopLocations.forEach(function(item) {
-				markers.push(new display_loc(item));
-			});
-		};
-		
-		this.clearClick = function() {
-				clearMarkers();
-		}
-	
+    var self = this;
+
+    var markers = ko.observableArray([]);
+
+    this.runClick = function() {
+
+        runLocations.forEach(function(item) {
+            markers.push(new display_loc(item));
+        });
+    }
+    this.eatClick = function() {
+        eatLocations.forEach(function(item) {
+            markers.push(new display_loc(item));
+        });
+    };
+
+
+    this.shopClick = function() {
+        shopLocations.forEach(function(item) {
+            markers.push(new display_loc(item));
+        });
+    };
+
+    this.clearClick = function() {
+        clearMarkers();
+    }
+
 };
 ko.applyBindings(new viewModel());
--- js/googlemapAPI.js
+++ js/googlemapAPI.js
@@ -1,6 +1,6 @@
-
-
-
+//create a global map variable for use in all functions
+//create a golbal markers array to store map markers
+//create the variable to store my neighborhood coordinates
 var map;
 var markers = [];
 var myTown = {
@@ -8,56 +8,62 @@ var myTown = {
     lng: -88.081
 };
 
-
+//create the map callback in HTML will run the initMap function
+//myTown is the center point on the map zoomed into 12 
+//addMarker is called passing on the coordinates and the name for the Marker
 function initMap() {
 
     var mapOptions = {
         center: myTown,
         zoom: 12
     };
-     map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-		addMarker(myTown, "Home");
-		}
+    addMarker(myTown, "Home");
+}
+//addMarker is the function that creates map markers
+//it takes 3 variables
+function addMarker(loc, name, icon) {
 
-	function addMarker(loc, name, icon) {
-
-	var location = new google.maps.LatLng(loc);
-
-		var marker = new google.maps.Marker({
-			position: loc,
-			icon: icon,
-			title: name,
-			map: map
-		}); 
-		markers.push(marker);
-	setMapOnAll(map);
-	}; 
-	
-	var display_loc = function(data) {
-	var self = this;
-	
- var lat = data.lat;
- var lng = data.lng;
-var mylatlng = { lat: lat , lng: lng};
-var myLocName = data.name;
-var myIcon = data.icon;
-
- addMarker(mylatlng, myLocName, myIcon);
-	
-
-	console.log("click ran");
+    var marker = new google.maps.Marker({
+        position: loc,
+        icon: icon,
+        title: name,
+        map: map
+    });
+    markers.push(marker);
+    setMapOnAll(map);
 };
+//object data is passed to display_loc function
+//the function pulls out and assigns the keys to variables
+//those variables are then passed to the addMarker function
+var display_loc = function(data) {
 
+    var lat = data.lat;
+    var lng = data.lng;
+    var mylatlng = {
+        lat: lat,
+        lng: lng
+    };
+    var myLocName = data.name;
+    var myIcon = data.icon;
+
+    addMarker(mylatlng, myLocName, myIcon);
+
+
+    console.log("click ran");
+};
+//setMapOnAll function enables visiablity of the markers on the map
 function setMapOnAll(map) {
-	  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
 
-  };
+    };
 };
 
-
+//it also passes on the null attribute to the setMapOnAll function to remove them from the map
+//clearMarkers removes the markers stored in the array markers by setting the array to empty
 function clearMarkers() {
-  setMapOnAll(null);
-	markers = [];
+    setMapOnAll(null);
+    markers = [];
 };
